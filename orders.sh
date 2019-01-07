010e52888041fc481efddf409740544a082103d7
Tbhangale <tejalbhangale15@gmail.com>
2019-01-07 15:47:06 -0500

    Add files via upload

---
+++ orders.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+D=`date --date="yesterday" +%Y-%m-%d`
+#D=2013-07-26
+
+sqoop import \
+--connect jdbc:mysql://ms.itversity.com:3306/retail_db \
+--username retail_user \
+--password itversity \
+--fields-terminated-by ',' \
+--target-dir /user/tbhangale/sqoop_import/retail_db/orders/${D} \
+-e "select * from orders where DATE(order_date)='${D}' AND \$CONDITIONS" \
+--split-by order_id
+
+hive -S -e 'use 'tbhangale'; select * from orders limit 5'
