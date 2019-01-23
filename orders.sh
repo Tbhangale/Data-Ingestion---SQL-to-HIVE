dde56aa848db1e1cd60f7efb61ac3ad972201640
Tbhangale <tejalbhangale15@gmail.com>
2019-01-23 15:35:55 -0500

    Update orders.sh

--- orders.sh
+++ orders.sh
@@ -4,9 +4,9 @@ D=`date --date="yesterday" +%Y-%m-%d`
 #D=2013-07-26
 
 sqoop import \
---connect jdbc:mysql://ms.itversity.com:3306/retail_db \
---username retail_user \
---password itversity \
+--connect jdbc:mysql://******/retail_db \
+--username ****** \
+--password ****** \
 --fields-terminated-by ',' \
 --target-dir /user/tbhangale/sqoop_import/retail_db/orders/${D} \
 -e "select * from orders where DATE(order_date)='${D}' AND \$CONDITIONS" \
